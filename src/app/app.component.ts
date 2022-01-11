4b041b05a6879f1f64cffb15ba355eb9618fea94
bartoszprojects <bartosz.projects1@gmail.com>
2022-01-11 14:49:25 +0100

    milliseconds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import {interval} from "rxjs";
 import {timer} from "rxjs";
 import {finalize, take} from "rxjs/operators";
 import {Observable} from "rxjs";
+import * as moment from "moment";
 
 @Component({
   selector: 'app-root',
@@ -13,13 +14,13 @@ import {Observable} from "rxjs";
 export class AppComponent implements OnInit{
   main_interval : any;
   small_interval: any;
+  how_long: number = 5000;
+  cycle_time_left: any;
 
   my_variable: any;
   foo = 'Hello';
   bar = 'World';
-  time: number = 0;
-  how_long: number = 1000;
-  time_long: number = 1000;
+
   constructor(private service: MainService) {
 
   }
@@ -27,18 +28,19 @@ export class AppComponent implements OnInit{
   ngOnInit(): void {
     this.service.getDataFromJsonFile().subscribe(res => console.log(res))
 
-    this.main_interval = timer(1000, 1000).pipe(take(10)).subscribe((res: any) => {
+    this.main_interval = timer(0, this.how_long).pipe(take(10)).subscribe((res: any) => {
       console.log('response from main_interval: ', res)
       this.smallInterval()
     })
   }
 
   smallInterval() {
-    this.time_long = 1000;
-    this.small_interval = timer(0,100).pipe(take(10)).subscribe((res:any) => {
-      this.time_long -= 100
-      console.log(this.time_long)
-      if (this.time_long == 0) {
+    this.cycle_time_left = this.how_long
+    let cycles_amount = this.how_long / 100
+    this.small_interval = timer(0,100).pipe(take(cycles_amount)).subscribe((res:any) => {
+      this.cycle_time_left -= 100
+      console.log(this.cycle_time_left)
+      if (this.cycle_time_left == 0) {
         console.log('---------------------------------')
 
 
@@ -49,7 +51,8 @@ export class AppComponent implements OnInit{
   stopSmallInterval() {
     this.small_interval.unsubscribe()
     this.main_interval.unsubscribe()
-    console.log('it takes: ', (1000 - this.time_long))
+    console.log(moment.duration(this.cycle_time_left).asSeconds())
+
   }
 
   changeFn(e: any) {

d7e9b0916874caf42ab3227c39bf49bc05283f4a
bartoszprojects <bartosz.projects1@gmail.com>
2022-01-14 16:42:21 +0100

    main time engine works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,25 +17,40 @@ export class AppComponent implements OnInit{
   how_long: number = 5000;
   cycle_time_left: any;
   current_word : string = '';
-  words : Array<any> = ['dog', 'cat', 'forest']
+  word_to_translate: string = '';
+  words : Array<any> = ['dog', 'cat', 'red', 'lala']
+  points : number = 0;
+  global_word_index: number = 0;
+  time_left : any;
 
-  my_variable: any;
-  foo = 'Hello';
-  bar = 'World';
 
   constructor(private service: MainService) {
-
   }
 
   ngOnInit(): void {
     this.service.getDataFromJsonFile().subscribe(res => console.log(res))
 
-    this.main_interval = timer(0, this.how_long).pipe(take(10)).subscribe((res: any) => {
+
+  }
+
+  startEverything() {
+    this.bigInterval()
+  }
+
+  // this function runs Big Interval - it means interval for list of words. Every cycle is single word
+  bigInterval() {
+    this.main_interval = timer(0, this.how_long).pipe(take(this.words.length - this.global_word_index))
+      .subscribe((res: any) => {
+
+      this.current_word = this.words[this.global_word_index]
+      this.global_word_index += 1
+
       console.log('response from main_interval: ', res)
       this.smallInterval()
     })
   }
 
+  // this function runs Small Interval - it means interval for every word: it's counting time inner single word
   smallInterval() {
     this.cycle_time_left = this.how_long
     let cycles_amount = this.how_long / 100
@@ -48,20 +63,24 @@ export class AppComponent implements OnInit{
     })
   }
 
-  stopSmallInterval() {
+  stopIntervals() {
     this.small_interval.unsubscribe()
     this.main_interval.unsubscribe()
     console.log('it takes: ', moment.duration(this.cycle_time_left).asSeconds())
 
   }
 
-  changeFn(e: any) {
-    console.log(e.target.value);
-  }
   modelChangeFn(value: any) {
-    if (value === 'dog') {
-      this.stopSmallInterval()
+    if (value === this.current_word) {
+      this.points += 1
+      this.word_to_translate = ''
+      this.stopIntervals()
+      this.startEverything()
     }
+  }
+
+  cleanx() {
+    this.word_to_translate = ''
   }
 
 }

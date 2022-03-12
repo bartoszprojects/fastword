e30e0f2096c9ea73dc3189d9fff0e9679b2a7d38
bartoszprojects <bartosz.projects1@gmail.com>
2022-03-12 22:00:48 +0100

    repaired, reworked and prepared whole code for DOCKER

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'back'
+    return 'bacfdask'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
---
+++ readme.md
@@ -0,0 +1,9 @@
+FASTWORD Project
+
+DOCKER IMAGES:
+- angular frontend
+- flask backend
+- postgresql database
+
+- docker-compose build
+- docker-compose up
